c8c925ce211c8e7442df54556742dc33dd596cff
Ukashatu <Ukashatui478@gmail.com>
2025-12-10 18:13:21 +0100

    Add missing vite-react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // This is the line that was failing
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
-});
+})
